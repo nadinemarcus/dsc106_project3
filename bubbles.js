20696d91aa3d94d41f51815529850b560335ee74
Paul Losiewicz <plosiewi@ucsd.edu>
2024-05-14 16:58:05 -0700

    u rock with it?

--- bubbles.js
+++ bubbles.js
@@ -1,198 +1,58 @@
 (function () {
-    var width = 1500;
-    height = 800;
+    var width = 1450,
+        height = 750;
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
     var svg = d3.select("#chart")
         .append("svg")
         .attr("height", height)
         .attr("width", width)
         .append("g")
-        .attr("transform", "translate(0,0)")
+        .attr("transform", "translate(0,0)");
 
-    var radiusScale = d3.scaleSqrt().domain([485930816, 2923706026]).range([5, 50])
+    var radiusScale = d3.scaleSqrt().domain([485930816, 2923706026]).range([4, 40]);
 
-    var forceXaction = d3.forceX(function (d) {
-        if (d.Action == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
+    // Define generic forceX function for genre separation
+    function createForceX(genre) {
+        return d3.forceX(function (d) {
+            return d[genre] == 1 ? 300 : 1000;
+        }).strength(0.1);  // Adjusted strength for better separation
+    }
 
-    var forceXbiography = d3.forceX(function (d) {
-        if (d.Biography == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXadventure = d3.forceX(function (d) {
-        if (d.Adventure == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXcomedy = d3.forceX(function (d) {
-        if (d.Comedy == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXanimation = d3.forceX(function (d) {
-        if (d.Animation == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXdrama = d3.forceX(function (d) {
-        if (d.Drama == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXfamily = d3.forceX(function (d) {
-        if (d.Family == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXcrime = d3.forceX(function (d) {
-        if (d.Crime == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXfantasy = d3.forceX(function (d) {
-        if (d.Fantasy == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXhistory = d3.forceX(function (d) {
-        if (d.History == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXhorror = d3.forceX(function (d) {
-        if (d.Horror == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXmusic = d3.forceX(function (d) {
-        if (d.Music == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXmusical = d3.forceX(function (d) {
-        if (d.Musical == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXmystery = d3.forceX(function (d) {
-        if (d.Mystery == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXromance = d3.forceX(function (d) {
-        if (d.Romance == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXscifi = d3.forceX(function (d) {
-        if (d.SciFi == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXthriller = d3.forceX(function (d) {
-        if (d.Thriller == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-    var forceXwestern = d3.forceX(function (d) {
-        if (d.Western == 1) {
-            return 300
-        } else {
-            return 1000
-        }
-    }).strength(0.05)
-
-
-    var forceXcombine = d3.forceX(width / 2).strength(0.05)
-
-    var forceCollide = d3.forceCollide(function (d) {
-        return radiusScale(d.sales) + 2
-    })
+    var forceXcombine = d3.forceX(width / 2).strength(0.05);
+    var forceY = d3.forceY((height / 2) + 100).strength(0.05);
+    var forceCollide = d3.forceCollide(d => radiusScale(d.sales) + 2);
 
     var simulation = d3.forceSimulation()
         .force("x", forceXcombine)
-        .force("y", d3.forceY(height / 2).strength(0.05))
-        .force("collide", forceCollide)
+        .force("y", forceY)
+        .force("collide", forceCollide);
 
     // Tooltip
     var tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
+    
 
     function ready(error, datapoints) {
-        var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-            .domain(datapoints.map(function (d) { return d.Distributor; }));
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory20)
+        .domain(datapoints.map(d => d.Distributor));
 
         var circles = svg.selectAll(".movie")
             .data(datapoints)
             .enter().append("circle")
             .attr("class", "movie")
-            .attr("r", function (d) {
-                return radiusScale(d.sales);
-            })
-            .attr("fill", function (d) {
-                // Assign color based on genre
-                return colorScale(d.Distributor);
-            })
+            .attr("r", d => radiusScale(d.sales))
+            .attr("fill", d => colorScale(d.Distributor))
             .on('mouseover', function (d) {
                 // Show tooltip on hover
                 tooltip.transition()
                     .duration(200)
                     .style("opacity", .9);
-                tooltip.html(d.Title + "<br/>" + "Sales: $" + d.sales)
+                tooltip.html(d.Title + "<br/>" + "Distributor: " + d.Distributor + "<br/>" + "Sales: $" + numberWithCommas(d.sales))
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY - 28) + "px");
             })
@@ -202,239 +62,84 @@
                     .duration(500)
                     .style("opacity", 0);
             });
-        simulation.nodes(datapoints)
-            .on('tick', ticked);
+
+        simulation.nodes(datapoints).on('tick', ticked);
 
         function ticked() {
             circles
-                .attr("cx", function (d) {
-                    return d.x;
-                })
-                .attr("cy", function (d) {
-                    return d.y;
-                });
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
         }
 
+        // Add legend
+        // Add legend
+        // Add legend
+        var legend = svg.selectAll(".legend")
+            .data(colorScale.domain())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", colorScale);
+
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d; });
+
+        // Add background for legend text
+        legend.each(function(d, i) {
+            var bbox = this.getBBox();
+            d3.select(this).insert("rect", "text")
+                .attr("x", bbox.x - 2)
+                .attr("y", bbox.y - 2)
+                .attr("width", bbox.width + 4)
+                .attr("height", bbox.height + 4)
+                .style("fill", colorScale(d))
+                .style("opacity", 0.9);
+        });
+
+
+        // Event listeners for buttons
+        function addButtonListener(buttonId, forceX) {
+            d3.select(buttonId).on('click', function () {
+                simulation
+                    .force("x", forceX)
+                    .alpha(0.5) // Reset alpha to restart the simulation
+                    .restart();
+            });
+        }
+
+        addButtonListener("#adventure", createForceX("Adventure"));
+        addButtonListener("#action", createForceX("Action"));
+        addButtonListener("#musical", createForceX("Musical"));
+        addButtonListener("#western", createForceX("Western"));
+        addButtonListener("#mystery", createForceX("Mystery"));
+        addButtonListener("#thriller", createForceX("Thriller"));
+        addButtonListener("#romance", createForceX("Romance"));
+        addButtonListener("#scifi", createForceX("SciFi"));
+        addButtonListener("#drama", createForceX("Drama"));
+        addButtonListener("#family", createForceX("Family"));
+        addButtonListener("#history", createForceX("History"));
+        addButtonListener("#fantasy", createForceX("Fantasy"));
+        addButtonListener("#biography", createForceX("Biography"));
+        addButtonListener("#crime", createForceX("Crime"));
+        addButtonListener("#comedy", createForceX("Comedy"));
+        addButtonListener("#horror", createForceX("Horror"));
+        addButtonListener("#music", createForceX("Music"));
+        addButtonListener("#animation", createForceX("Animation"));
+        addButtonListener("#combined", forceXcombine);
+
+        simulation.nodes(datapoints).on('tick', ticked);
     }
 
     d3.queue()
         .defer(d3.csv, "imdb2.csv")
-        .await(ready)
-
-    function ready(error, datapoints) {
-        var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-            .domain(datapoints.map(function (d) { return d.Distributor; }));
-
-        var circles = svg.selectAll(".movie")
-            .data(datapoints)
-            .enter().append("circle")
-            .attr("class", "movie")
-            .attr("r", function (d) {
-                return radiusScale(d.sales)
-            })
-            .attr("fill", function (d) {
-                // Assign color based on genre
-                return colorScale(d.Distributor);
-            })
-            .on('click', function (d) {
-                console.log(d)
-            })
-            .on('mouseover', function (d) {
-                // Show tooltip on hover
-                tooltip.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                tooltip.html(d.Title + "<br/>" + "Sales: $" + d.sales)
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-            })
-
-        d3.select("#adventure").on('click', function () {
-            simulation
-                .force("x", forceXadventure)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#action").on('click', function () {
-            simulation
-                .force("x", forceXaction)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#musical").on('click', function () {
-            simulation
-                .force("x", forceXmusical)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#western").on('click', function () {
-            simulation
-                .force("x", forceXwestern)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-
-        d3.select("#mystery").on('click', function () {
-            simulation
-                .force("x", forceXmystery)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-
-        d3.select("#thriller").on('click', function () {
-            simulation
-                .force("x", forceXthriller)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#romance").on('click', function () {
-            simulation
-                .force("x", forceXromance)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#scifi").on('click', function () {
-            simulation
-                .force("x", forceXscifi)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#drama").on('click', function () {
-            simulation
-                .force("x", forceXdrama)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#family").on('click', function () {
-            simulation
-                .force("x", forceXfamily)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#history").on('click', function () {
-            simulation
-                .force("x", forceXhistory)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#fantasy").on('click', function () {
-            simulation
-                .force("x", forceXfantasy)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#biography").on('click', function () {
-            simulation
-                .force("x", forceXbiography)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#crime").on('click', function () {
-            simulation
-                .force("x", forceXcrime)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-
-        d3.select("#comedy").on('click', function () {
-            simulation
-                .force("x", forceXcomedy)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#horror").on('click', function () {
-            simulation
-                .force("x", forceXhorror)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#music").on('click', function () {
-            simulation
-                .force("x", forceXmusic)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#animation").on('click', function () {
-            simulation
-                .force("x", forceXanimation)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-
-        })
-
-        d3.select("#combined").on('click', function () {
-            simulation
-                .force("x", forceXcombine)
-                .force("collide", forceCollide)
-                .alphaTarget(0.1)
-                .restart()
-        })
-
-        simulation.nodes(datapoints)
-            .on('tick', ticked)
-
-        function ticked() {
-            circles
-                .attr("cx", function (d) {
-                    return d.x
-                })
-                .attr("cy", function (d) {
-                    return d.y
-                })
-        }
-
-    }
-
+        .await(ready);
 })();
